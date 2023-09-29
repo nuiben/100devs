02e098109c183067e8d6ba3d90993777d6bf60ed
Ben Porter <102399623+nuiben@users.noreply.github.com>
2023-09-29 19:18:42 -0400

    class-12

---
+++ class-12-js/class-weekend-boring/js/main.js
@@ -0,0 +1,21 @@
+document.querySelector('#check').addEventListener('click', check)
+
+function check() {
+
+  const day = document.querySelector('#day').value.toLowerCase();
+
+  //Conditionals go here
+  if (day === "tuesday" || "thursday" === day){
+    setText("100 Devs Class Tonight!");
+  } else if (day === "saturday" || day === "sunday") {
+    setText('Weekend!')
+  } else {
+    setText('Boring Day');
+  }
+
+}
+
+
+function setText(string){
+  document.getElementById('placeToSee').innerHTML = string;
+}
