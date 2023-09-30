e9f21272bfe70fbe269c0aa12a39ad8e2062a199
Ben Porter <102399623+nuiben@users.noreply.github.com>
2023-09-30 01:48:55 -0400

    arrow-functions task

---
+++ javascript-info-tasks/functions/arrow-function.js
@@ -0,0 +1,26 @@
+// Rewrite with Arrow Functions
+
+function ask(question, yes, no) {
+    if (confirm(question)) yes();
+    else no();
+}
+
+ask(
+    "Do you agree?",
+    function() { alert("You agreed."); },
+    function() { alert("You canceled the execution."); }
+);
+
+
+// Rewritten:
+
+function ask(question, yes, no) {
+    if(confirm(question)) yes();
+    else no();
+}
+
+ask(
+    "Do you agree?",
+    () => alert("You agreed."),
+    () => alert("You canceled the execution.")
+);
