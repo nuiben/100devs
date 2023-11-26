0893b90b81c881c706fabccf9cf10dd40b6ea25a
Ben Porter <102399623+nuiben@users.noreply.github.com>
2023-11-26 17:56:59 -0500

    initial API hw checkpoint - got carosel working

---
+++ API - Class 28/QuizAPI/js/main.js
@@ -0,0 +1,45 @@
+// const btn = document.querySelector('.btn');
+
+// btn.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     document.querySelector('#my-form').computedStyleMap.background = '#000000';
+//     document.querySelector('body').classList.add('bg-dark');
+//     document.querySelector('.items').lastElementChild.innerHTML = '<h1>Waffles!!</h1>';
+// })
+
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+    if(nameInput.value === '' || emailInput.value === '') {
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter a value for all fields';
+
+        setTimeout(() => msg.remove(), 3000);
+    } else {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+        userList.appendChild(li);
+
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+
+}
+
+
+// console.log('Hello?');
+// console.log(document.getElementsByClassName('item'));
+
+// const ul = document.querySelector('.items');
+
+// ul.remove();
+// ul.lastElementChild.remove();
+// ul.firstElementChild.textContent = 'Beans';
---
+++ API - Class 28/cocktailDB/js/main.js
@@ -0,0 +1,64 @@
+//The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+// https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
+document.querySelector('button').addEventListener('click', getDrink)
+
+function hasWhiteSpace(s) {
+    return s.indexOf(' ') >= 0;
+}
+
+
+function getDrink() {
+    let drink = document.querySelector('input').value
+    if (hasWhiteSpace(drink)) {
+        drink = drink.replace(" ", "%20")
+    }
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data.drinks)
+        // document.querySelector('h2').innerText = data.drinks[0].strDrink
+        // document.querySelector('img').src = data.drinks[0].strDrinkThumb
+        // document.querySelector('h3').innerText = data.drinks[0].strInstructions
+        createCarouselItems(data.drinks);
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    })
+
+}
+
+function createCarouselItems(items) {
+    const carouselContainer = document.getElementById('carousel');
+
+    items.forEach((item, index) => {
+      const carouselItem = document.createElement('div');
+      carouselItem.className = 'carousel-item' + (index === 0 ? ' active' : '');
+      const image = document.createElement('img');
+      image.src = item.strDrinkThumb;
+      image.alt = item.strDrink;
+    //   document.querySelector('h2').innerText = image.alt;
+
+      carouselItem.appendChild(image);
+      carouselContainer.appendChild(carouselItem);
+    });
+}
+
+
+let index = 0;
+
+function showNextItem() {
+  let items = document.querySelectorAll('.carousel-item');
+  if (items.length == 0) return;
+
+  // Remove 'active' class from current item
+  items[index].classList.remove('active');
+
+  // Increment index
+  index = (index + 1) % items.length;
+
+  // Add 'active' class to next item
+  items[index].classList.add('active');
+}
+
+// Set interval for carousel to change
+setInterval(showNextItem, 3000); // Change every 3 seconds
