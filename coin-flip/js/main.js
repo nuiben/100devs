2d160144dcc9d7a317c9fde558c0ca12f316d884
Ben Porter <102399623+nuiben@users.noreply.github.com>
2023-12-06 16:48:34 -0500

    started coinflip hw

---
+++ coin-flip/js/main.js
@@ -0,0 +1,6 @@
+//Create ane event listener for when the button is clicked
+document.querySelector('button').addEventListener("click", getFetch());
+
+function getFetch() {
+
+}
--- coin-flip/server.js
+++ coin-flip/server.js
@@ -1,9 +1,19 @@
 const http = require('http')
 const fs = require('fs')
+
 http.createServer((req, res) => {
   fs.readFile('coinflip.html', (err, data) => {
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.write(data)
-    res.end()
+    if (err) {
+      res.writeHead(500);
+      res.write('Error reading file');
+      res.end();
+    } else {
+      const randomChoice = Math.round(Math.random()); // 0 or 1
+      const outcome = randomChoice === 0 ? 'Heads' : 'Tails';
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      // res.write(`<p>Outcome: ${outcome}</p>`); // Display the outcome on the page
+      res.end();
+    }
   })
 }).listen(8000)
