cdee7268927d3caea093de341f358271b511bd9d
Ben Porter <102399623+nuiben@users.noreply.github.com>
2023-12-06 19:37:56 -0500

    coin-flipper complete :)

--- coin-flip/js/main.js
+++ coin-flip/js/main.js
@@ -1,6 +1,11 @@
 //Create ane event listener for when the button is clicked
-document.querySelector('button').addEventListener("click", getFetch());
+document.querySelector('button').addEventListener("click", getFetch);
 
-function getFetch() {
+async function getFetch() {
+    const res = await fetch(`/api`)
+    const data = await res.json()
 
-}
+    console.log(data);
+    document.querySelector("span").textContent = data.value
+    document.querySelector("img").src="./assets/heads.jpg";
+  }
--- coin-flip/server.js
+++ coin-flip/server.js
@@ -1,19 +1,88 @@
-const http = require('http')
-const fs = require('fs')
+// const http = require('http');
+// const url = require('url');
+// const fs = require('fs');
+// const figlet = require('figlet');
 
-http.createServer((req, res) => {
-  fs.readFile('coinflip.html', (err, data) => {
-    if (err) {
-      res.writeHead(500);
-      res.write('Error reading file');
-      res.end();
-    } else {
-      const randomChoice = Math.round(Math.random()); // 0 or 1
-      const outcome = randomChoice === 0 ? 'Heads' : 'Tails';
+// http.createServer((req, res) => {
+//   const page = url.parse(req.url).pathname;
+//   fs.readFile('coinflip.html', (err, data) => {
+//     if (page == '/') {
+//       fs.readFile('index.html', function(data) {
+//         res.writeHead(200, {'Content-Type': 'text/html'});
+//         res.write(data);
+//         res.end();
+//       });
+//     } else if (page == '/api') {
+//       const randomChoice = Math.round(Math.random()); // 0 or 1
+//       const outcome = randomChoice === 0 ? 'Heads' : 'Tails';
+//       const objToJson = {
+//         value: outcome
+//       }
+//       res.end(JSON.stringify(objToJson));
+//     } else {
+//       figlet('404!!', function(err, data) {
+//         if (err) {
+//             console.log('Something went wrong...');
+//             console.dir(err);
+//             return;
+//         }
+//         res.write(data);
+//         res.end();
+//       });
+//     }
+//   })
+// }).listen(8000)
+
+
+const http = require('http');
+const fs = require('fs')
+const url = require('url');
+// const querystring = require('querystring');
+const figlet = require('figlet')
+
+const server = http.createServer((req, res) => {
+  const page = url.parse(req.url).pathname;
+  console.log(page);
+  if (page == '/') {
+    fs.readFile('coinflip.html', function(err, data) {
       res.writeHead(200, {'Content-Type': 'text/html'});
+      console.log(typeof(data))
       res.write(data);
-      // res.write(`<p>Outcome: ${outcome}</p>`); // Display the outcome on the page
       res.end();
+    });
+  }
+  else if (page == '/api') {
+    const randomChoice = Math.round(Math.random()); // 0 or 1
+    const outcome = randomChoice === 0 ? 'Heads' : 'Tails';
+    const objToJson = {
+      value: outcome
     }
-  })
-}).listen(8000)
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.write(JSON.stringify(objToJson));
+    res.end();
+  }
+  else if (page == '/css/style.css'){
+    fs.readFile('css/style.css', function(err, data) {
+      res.write(data);
+      res.end();
+    });
+  }else if (page == '/js/main.js'){
+    fs.readFile('js/main.js', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/javascript'});
+      res.write(data);
+      res.end();
+    });
+  } else {
+    figlet('404!!', function(err, data) {
+      if (err) {
+          console.log('Something went wrong...');
+          console.dir(err);
+          return;
+      }
+      res.write(data);
+      res.end();
+    });
+  }
+});
+
+server.listen(8000);
